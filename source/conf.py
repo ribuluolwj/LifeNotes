f8b75401005ad80e758cf4ad938ce65a59b74412
ribuluolwj <renpengbuaa@outlook.com>
2023-10-27 22:58:44 +0800

    Add theme dir

--- source/conf.py
+++ source/conf.py
@@ -29,5 +29,6 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ['./_theme']
 html_static_path = ['_static']
