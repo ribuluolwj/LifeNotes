a56e8a18bc2ef32cf636809bcde4594a13dee9b1
ribuluolwj <renpengbuaa@outlook.com>
2023-10-27 23:29:00 +0800

    Add extensions requirements

--- source/conf.py
+++ source/conf.py
@@ -13,9 +13,6 @@ release = 'v1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-import os
-import sys
-sys.path.insert(0,os.path.abspath('./_extensions'))
 extensions = [
         'myst_parser',
 #        'recommonmark',
